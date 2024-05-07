b132a5ae465c421d60312f23735f1ce512301689
Pedro Teodoro <102976793+Teo442@users.noreply.github.com>
2024-05-07 20:54:32 -0300

    Add files via upload

---
+++ java/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package java; //pacote
+
+/**
+ * @author Teo442
+ */
+
+//classe 
+public class PrimeiroPrograma{ 
+    //método principal
+    public static void main(String[] args){
+        System.out.println("Hello World"); //imprime na tela do usuário
+    }
+}
